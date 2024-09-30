e4b39dbccd63858da3133480c9b781d83fd36ad2
Natasha <kashitsyna.2001@mail.ru>
2024-09-30 19:34:07 +0400

    add colors

--- src/components/AProduct/src/AProduct.vue
+++ src/components/AProduct/src/AProduct.vue
@@ -4,6 +4,25 @@
       <p>{{ productData.title }}</p>
       <p>{{ productData.brandName }}</p>
       <p>{{ currencySign(productData.regular_price.currency) }}{{ productData.regular_price.value }}</p>
+
+      <div v-if="productData.configurable_options !== undefined">
+        <div v-for="(option, optionIndex) in productData.configurable_options">
+
+          <div v-if="option.attribute_code === 'color'">
+            <div class="product__colors">
+              <div v-for="(item, itemIndex) in option.values">
+                <div :style="{ backgroundColor: item.value }" class="product__color"></div>
+              </div>
+            </div>
+            
+          </div>
+
+          <div v-if="option.attribute_code === 'size'">
+
+          </div>
+
+        </div>
+      </div>
     </div>
   </template>
   
@@ -40,5 +59,13 @@
 <style scoped>
 .product__wrapper {
   display: block;
+}
+.product__colors {
+  display: flex;
+  gap: 20px;
+}
+.product__color {
+  width: 30px; 
+  height: 20px;
 }
 </style>
